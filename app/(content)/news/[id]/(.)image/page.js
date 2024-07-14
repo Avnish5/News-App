0ca63d691b336c6e45638d0c9618c4aaafc16e87
Avnish Kumar <Avnein4988@gmail.com>
2024-07-14 15:08:13 +0530

    implemeted the conecpt of routing group

--- app/(content)/news/[id]/(.)image/page.js
+++ app/(content)/news/[id]/(.)image/page.js
@@ -1,18 +1,26 @@
+'use client'
 import { DUMMY_NEWS } from "@/dummy-news";
+import { useRouter } from "next/navigation";
 
 export default function InterceptedFullImage({params})
 {
    const newsItemSlug=params.id;
    const newsItem =DUMMY_NEWS.find((newsItem) => newsItem.slug === newsItemSlug);
 
+   const router=useRouter();
+
   if (!newsItem) {
     notFound();
   }
    return(
        <>
+       <div className="modal-backdrop" onClick={router.back}/>
+       <dialog className="modal" open>
        <h2>Intercepted image</h2>
        <div className="fullscreen-image">
          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title}/>
-       </div></>
+       </div>
+       </dialog>
+       </>
    )
 }
